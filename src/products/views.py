33b04588c7c0211c18af1db3341a75a5e5282334
Mateusz Nowak <mateusz.nowak@nokia.com>
2021-05-19 16:10:33 +0200

    raw HTML form

--- src/products/views.py
+++ src/products/views.py
@@ -8,14 +8,26 @@ from .models import Product
 
 
 def product_create_view(request):
-	form = ProductForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-		form = ProductForm()
-	context = {
-		'form': form
-	}
+	# print("request GET:", request.GET)
+	# print("request POST:", request.POST)
+	if request.method == "POST":
+		my_title = request.POST.get('title')
+		print(my_title)
+	
+	context = {}
 	return render(request, "products/product_create.html", context)
+
+
+
+# def product_create_view(request):
+# 	form = ProductForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
+# 		form = ProductForm()
+# 	context = {
+# 		'form': form
+# 	}
+# 	return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
